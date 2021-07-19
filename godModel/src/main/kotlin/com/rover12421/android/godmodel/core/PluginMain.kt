cc67532c87e630d18f3a7952067cb607c7115571
Rover12421 <rover12421@163.com>
2021-07-19 22:01:36 +0800

    GodModel add LibraryExtension support

--- godModel/src/main/kotlin/com/rover12421/android/godmodel/core/PluginMain.kt
+++ godModel/src/main/kotlin/com/rover12421/android/godmodel/core/PluginMain.kt
@@ -1,13 +1,15 @@
 package com.rover12421.android.godmodel.core
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class PluginMain : Plugin<Project> {
     override fun apply(project: Project) {
-        val appExtension = project.extensions.findByType(AppExtension::class.java) ?: return
-        appExtension.registerTransform(GodModelTransform(project))
+        val aandroidExtension = project.extensions.findByType(AppExtension::class.java)
+            ?: project.extensions.findByType(LibraryExtension::class.java) ?: return
+        aandroidExtension.registerTransform(GodModelTransform(project))
         println(">>>>>>>> godmodel.core PluginMain")
     }
 }
