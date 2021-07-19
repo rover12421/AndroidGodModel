26cfad29789577097d715dc65e82455259ae8396
Rover12421 <rover12421@163.com>
2021-07-19 22:22:11 +0800

    fix GodModel LibraryExtension getAndroidJarPath NullPointerException

--- godModel/src/main/kotlin/com/rover12421/android/godmodel/core/util/ClassLoaderHelper.kt
+++ godModel/src/main/kotlin/com/rover12421/android/godmodel/core/util/ClassLoaderHelper.kt
@@ -2,15 +2,21 @@ package com.rover12421.android.godmodel.core.util
 
 import com.android.build.api.transform.TransformInput
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Project
 import java.io.File
+import java.lang.RuntimeException
 import java.net.URLClassLoader
 
 object ClassLoaderHelper {
 
     private fun getAndroidJarPath(project: Project): String {
-        val appExtension = project.extensions.findByType(AppExtension::class.java)!!
-        return "${appExtension.sdkDirectory}${File.separator}platforms${File.separator}${appExtension.compileSdkVersion}${File.separator}android.jar"
+        val appExtension = project.extensions.findByType(AppExtension::class.java) ?: project.extensions.findByType(LibraryExtension::class.java)
+        if (appExtension != null) {
+            return "${appExtension.sdkDirectory}${File.separator}platforms${File.separator}${appExtension.compileSdkVersion}${File.separator}android.jar"
+        } else {
+            throw RuntimeException("Not Found AppExtension/LibraryExtension !!!")
+        }
     }
 
     private fun checkInputs(inputs: Collection<TransformInput>, libs: MutableList<File>) {
